34dbf8910821d01b0c66eb82842e1155e024026e
luckybsmith <50602736+luckybsmith@users.noreply.github.com>
2022-10-06 17:36:44 +0900

    Add files via upload

---
+++ CustomSwitch/CustomSwitch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomSwitch
+//
+//  Created by 이송은 on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //constraint -> x to +75
+    //add animation
+    
+    @IBAction func btnClick(_ sender: Any) {
+    }
+    
+}
+
